4b2c319f62d91ecd5a23b503b1c64bcb3bd8bbc3
oskman <42803141+oskman@users.noreply.github.com>
2026-09-03 18:20:59 +0200

    Feat: simple live terminal display with escape codes

--- include/winUtils.hpp
+++ include/winUtils.hpp
@@ -7,6 +7,6 @@
 
 namespace skirnir::win {
     inline ULONGLONG FileTime_to_uint(const FILETIME& filetime) {
-        return (static_cast<ULONGLONG>(filetime.dwHighDateTime) << 32) | filetime.dwLowDateTime; //highdatetime er 32 bit, så caster til 64 bit usignert type, flytter deretter highdate bitene 32 plasser til venstre. Nå er halvparten til høyre bare 0-ere, henter deretter 32 bit lowdatetime og "or-er" de mot alle 0 bitene på høyre halvdel. Hvis 1 i lowdate blir 1or0=1, hvis 0 i lowdate blir 0or0=0 (bitwise or).
+        return (static_cast<ULONGLONG>(filetime.dwHighDateTime) << 32) | filetime.dwLowDateTime; //highdatetime er 32 bit, så caster til 64 bit usignert type (cast endrer datatype, lager et 64 bit objekt som bare er 0-ere og fyller inn de 32 bitene fra highdatetime), flytter deretter highdate bitene 32 plasser til venstre. Nå er halvparten til høyre bare 0-ere, henter deretter 32 bit lowdatetime og "or-er" de mot alle 0 bitene på høyre halvdel. Hvis 1 i lowdate blir 1or0=1, hvis 0 i lowdate blir 0or0=0 (bitwise or).
 }
 }
--- skirnir/main.cpp
+++ skirnir/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "systemMonitor.hpp"
+
+
+#define CLEAR_SCREEN "\033[2J"
+#define MOVE_CURSOR_HOME "\033[H"
+#define HIDE_CURSOR "\033[?25l"
+#define SHOW_CURSOR "\033[?25h"
 
 int main()
 {
 	skirnir::SystemMonitor sysmon;
 
-    std::cout << "Memory usage: " << sysmon.get_memory_usage() << "%" << '\n';
-    std::cout << "CPU usage: " << sysmon.get_cpu_usage() << "%" << '\n';
+    std::cout << HIDE_CURSOR;
+
+    std::cout << CLEAR_SCREEN;
+    for (int i = 0; i < 50; ++i) {
+        std::cout << MOVE_CURSOR_HOME;
+        std::cout << "===============================\n";
+        std::cout << "         SKIRNIR MONITOR       \n";
+        std::cout << "===============================\n";
+        std::cout << "Memory usage: " << sysmon.get_memory_usage() << "%" << '\n';
+        std::cout << "CPU usage: " << sysmon.get_cpu_usage() << "%" << '\n';
+        std::cout << "===============================\n";
+
+        std::cout.flush();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 	return 0;
 }
